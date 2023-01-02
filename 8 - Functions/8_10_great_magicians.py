721b61f8240669cefc4c95a9c2d72e7ae64021c0
Yago <yago.lima.lins@protonmail.com>
2023-01-02 02:16:50 -0300

    New exercises solved

---
+++ 8 - Functions/8_10_great_magicians.py
@@ -0,0 +1,24 @@
+magicians = ['houdini', 'copperfield', 'henning']
+great_magicians = []
+
+
+def show_magicians(magicians_list):
+    for magician in magicians_list:
+        print(magician.title())
+
+
+show_magicians(magicians)
+
+
+def make_great(magicians_list):
+    while magicians_list:
+        current_magician = magicians_list.pop()
+        great_magician = "Great " + current_magician
+        great_magicians.append(great_magician)
+        if not magicians_list:
+            break
+
+
+make_great(magicians)
+print(magicians)
+show_magicians(great_magicians)
---
+++ 8 - Functions/8_9_magicians.py
@@ -0,0 +1,7 @@
+magicians = ['houdini', 'copperfield', 'henning']
+
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician)
+
+show_magicians(magicians)
